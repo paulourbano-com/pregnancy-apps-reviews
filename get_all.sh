27994098d4187fdc37388b54ec4e57a58e3b687a
Paulo Urbano <paulo@paulourbano.com>
2019-06-04 02:55:18 +0000

    Scripts para processamento de dados

---
+++ get_all.sh
@@ -0,0 +1,64 @@
+./call.sh com.easymobs.pregnancy pt_BR rating 10 120
+./call.sh com.easymobs.pregnancy pt_BR helpfulness 10 120
+./call.sh com.easymobs.pregnancy pt_BR newest 10 120
+
+./call.sh com.easymobs.pregnancy pt rating 10 120
+./call.sh com.easymobs.pregnancy pt helpfulness 10 120
+./call.sh com.easymobs.pregnancy pt newest 10 120
+
+./call.sh com.easymobs.pregnancy en_GB rating 10 120
+./call.sh com.easymobs.pregnancy en_GB helpfulness 10 120
+./call.sh com.easymobs.pregnancy en_GB newest 10 120
+
+./call.sh com.easymobs.pregnancy en rating 10 120
+./call.sh com.easymobs.pregnancy en helpfulness 10 120
+./call.sh com.easymobs.pregnancy en newest 10 120
+
+./call.sh com.easymobs.pregnancy es rating 10 120
+./call.sh com.easymobs.pregnancy es helpfulness 10 120
+./call.sh com.easymobs.pregnancy es newest 10 120
+
+
+#---------------------------------------------------------
+./call.sh com.ovuline.pregnancy pt_BR rating 10 120
+./call.sh com.ovuline.pregnancy pt_BR helpfulness 10 120
+./call.sh com.ovuline.pregnancy pt_BR newest 10 120
+
+./call.sh com.ovuline.pregnancy pt rating 10 120
+./call.sh com.ovuline.pregnancy pt helpfulness 10 120
+./call.sh com.ovuline.pregnancy pt newest 10 120
+
+./call.sh com.ovuline.pregnancy en rating 10 120
+./call.sh com.ovuline.pregnancy en helpfulness 10 120
+./call.sh com.ovuline.pregnancy en newest 10 120
+
+./call.sh com.ovuline.pregnancy en_GB rating 10 120
+./call.sh com.ovuline.pregnancy en_GB helpfulness 10 120
+./call.sh com.ovuline.pregnancy en_GB newest 10 120
+
+./call.sh com.ovuline.pregnancy es rating 10 120
+./call.sh com.ovuline.pregnancy es helpfulness 10 120
+./call.sh com.ovuline.pregnancy es newest 10 120
+
+
+#---------------------------------------------------------
+./call.sh com.babycenter.pregnancytracker pt_BR rating 10 120
+./call.sh com.babycenter.pregnancytracker pt_BR helpfulness 10 120
+./call.sh com.babycenter.pregnancytracker pt_BR newest 10 120
+
+./call.sh com.babycenter.pregnancytracker pt rating 10 120
+./call.sh com.babycenter.pregnancytracker pt helpfulness 10 120
+./call.sh com.babycenter.pregnancytracker pt newest 10 120
+
+./call.sh com.babycenter.pregnancytracker en rating 10 120
+./call.sh com.babycenter.pregnancytracker en helpfulness 10 120
+./call.sh com.babycenter.pregnancytracker en newest 10 120
+
+./call.sh com.babycenter.pregnancytracker en_GB rating 10 120
+./call.sh com.babycenter.pregnancytracker en_GB helpfulness 10 120
+./call.sh com.babycenter.pregnancytracker en_GB newest 10 120
+
+./call.sh com.babycenter.pregnancytracker es rating 10 120
+./call.sh com.babycenter.pregnancytracker es helpfulness 10 120
+./call.sh com.babycenter.pregnancytracker es newest 10 120
+
---
+++ process_data.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import json
+import glob
+import copy
+
+def add_data(input_dictionary, data):
+  temp = copy.copy(input_dictionary)
+#   print(input_dictionary, data)
+  temp['language_code'] = data
+  return(temp)
+
+
+def get_dictionary_list(input_string, file_name):
+  
+  result = []
+  
+  lang_code = ''
+  if 'pt_BR' in file_name:
+    lang_code = 'pt_BR'
+  elif 'en_GB' in file_name:
+    lang_code = 'en_GB'
+  elif 'pt' in file_name:
+    lang_code = 'pt'
+  elif 'es' in file_name:
+    lang_code = 'es'
+  elif 'fr' in file_name:
+    lang_code = 'fr'
+  elif 'de' in file_name:
+    lang_code = 'de'
+  elif 'en' in file_name:
+    lang_code = 'en'
+
+  
+  json_str = '[' + input_string.replace('\n', '')
+
+
+  json_str = json_str.replace('\"', '')
+  json_str = json_str.replace('\t', '')
+  json_str = json_str.replace('undefined', '\'\'')
+  json_str = json_str.replace('\'', '\"')
+
+
+
+  json_str = json_str.replace('id:', '\"id\":')
+  json_str = json_str.replace('userName:', '\"userName\":')
+  json_str = json_str.replace('userImage:', '\"userImage\":')
+  json_str = json_str.replace('date:', '\"date\":')
+  json_str = json_str.replace('url:', '\"url\":')
+  json_str = json_str.replace('score:', '\"score\":')
+  json_str = json_str.replace('title:', '\"title\":')
+  json_str = json_str.replace('text:', '\"text\":')
+  json_str = json_str.replace('replyDate:', '\"replyDate\":')
+  json_str = json_str.replace('replyText:', '\"replyText\":')
+
+  try:
+    result = json.loads(json_str)
+    result = list(map(add_data, result, len(result) * [lang_code]))
+  except:
+    pass
+  
+  return(result)
+
+def process_app(app_id_str):
+  
+  file_list = glob.glob("pregnancy-apps-reviews/raw_data/*.txt")
+
+  rows_df = []
+
+  for file_name in file_list:
+    
+    if app_id_str in file_name:
+      print(file_name)
+      contents = []
+      with open(file_name, 'r') as file_handle:
+        contents = file_handle.read()
+
+      pages = contents.split('[')
+
+      size_before = len(rows_df)
+      for page in pages:
+        result = get_dictionary_list(page, file_name)
+        rows_df = rows_df + result
+
+  reviews_df = pd.DataFrame(rows_df)
+  print(reviews_df.info())
+  unique_ids = reviews_df[['id']].drop_duplicates().index
+  print(reviews_df[reviews_df.index.isin(unique_ids)].info())
+  filtered_df = reviews_df[reviews_df.index.isin(unique_ids)]
+  filtered_df = filtered_df[['userName',	'language_code',	'date',	'score',	'title',	'text',	'replyDate',	'replyText']]
+
+  filtered_df.to_csv('reviews_{}.csv'.format(app_id_str), index=False)
+  
+for app_id in ['com.easymobs.pregnancy', 'com.ovuline.pregnancy', 'com.babycenter.pregnancytracker']:
+  process_app(app_id)
